3f83752113d8ccd8d42fb45f9fb85bf1a0ba7ea4
thepoy <thepoy@163.com>
2023-03-20 18:55:25 +0800

    fix an error

--- docx/styles/styles.pyi
+++ docx/styles/styles.pyi
@@ -4,7 +4,7 @@
 # @Email:       thepoy@163.com
 # @File Name:   styles.pyi
 # @Created At:  2023-03-20 18:39:16
-# @Modified At: 2023-03-20 18:46:09
+# @Modified At: 2023-03-20 18:55:13
 # @Modified By: thepoy
 
 from docx.shared import ElementProxy
@@ -14,6 +14,6 @@ StyleType = ParagraphStyle | CharacterStyle | TableStyle | NumberingStyle
 
 class Styles(ElementProxy):
     def __contains__(self, name: str) -> bool: ...
-    def __getitem__(self, key: str) -> None: ...
+    def __getitem__(self, key: str) -> StyleType: ...
     def __iter__(self) -> StyleType: ...
     def __len__(self) -> int: ...
