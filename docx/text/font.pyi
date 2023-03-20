be983350abcbdd8936a0b96bec15fa61d9789d28
thepoy <thepoy@163.com>
2023-03-20 18:15:13 +0800

    update font.pyi

--- docx/text/font.pyi
+++ docx/text/font.pyi
@@ -4,9 +4,10 @@
 # @Email:       thepoy@163.com
 # @File Name:   font.pyi
 # @Created At:  2023-03-20 16:41:45
-# @Modified At: 2023-03-20 16:48:01
+# @Modified At: 2023-03-20 18:14:52
 # @Modified By: thepoy
 
+from docx.dml.color import ColorFormat
 from docx.shared import ElementProxy, Pt
 
 class Font(ElementProxy):
@@ -14,3 +15,5 @@ class Font(ElementProxy):
     def size(self) -> Pt: ...
     @size.setter
     def size(self, emu: Pt) -> None: ...
+    @property
+    def color(self) -> ColorFormat: ...
