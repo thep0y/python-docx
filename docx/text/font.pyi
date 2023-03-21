54ede6357cb17feea7370c39e8f738d5ecb0c553
thepoy <thepoy@163.com>
2023-03-21 14:16:16 +0800

    update font.pyi

--- docx/text/font.pyi
+++ docx/text/font.pyi
@@ -4,10 +4,11 @@
 # @Email:       thepoy@163.com
 # @File Name:   font.pyi
 # @Created At:  2023-03-20 16:41:45
-# @Modified At: 2023-03-20 18:30:05
+# @Modified At: 2023-03-21 14:16:01
 # @Modified By: thepoy
 
 from docx.dml.color import ColorFormat
+from docx.enum.base import EnumValue
 from docx.shared import ElementProxy, Pt
 
 class Font(ElementProxy):
@@ -21,3 +22,7 @@ class Font(ElementProxy):
     def name(self) -> str: ...
     @name.setter
     def name(self, value: str) -> None: ...
+    @property
+    def highlight_color(self) -> EnumValue: ...
+    @highlight_color.setter
+    def highlight_color(self, value: EnumValue) -> None: ...
